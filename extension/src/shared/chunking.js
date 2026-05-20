90b7b9d5c387cff0af217c1e863b4a6e62348104
sanjiv kumar <piyush20240109644@asose.in>
2026-05-20 20:54:01 +0530

    Delete extension directory

--- extension/src/shared/chunking.js
+++
@@ -1,9 +0,0 @@
-export function chunkText(text, maxChars = 1800) {
-  const chunks = [];
-  let cursor = 0;
-  while (cursor < text.length) {
-    chunks.push(text.slice(cursor, cursor + maxChars));
-    cursor += maxChars;
-  }
-  return chunks;
-}
